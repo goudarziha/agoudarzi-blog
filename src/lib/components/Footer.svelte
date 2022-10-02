0bc5b8d3189a05000858f6a7cd2ce55f78adac9b
unknown <goudarziha@gmail.com>
2022-10-02 14:05:07 -0400

    post 4

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,27 +1,41 @@
+<script>
+	const date = new Date();
+	const year = date.getFullYear();
+</script>
+
 <footer>
-	<p>Follow me</p>
-
-	<ul>
-		<li>
-			<a href="https://twitter.com/goudarziha">Twitter</a>
-		</li>
-		<li>
-			<a href="https://instagram.com/goudarziha">Instagram</a>
-		</li>
-		<li>
-			<a href="/">RSS</a>
-		</li>
-	</ul>
-
-	<p>Get in touch via email <a href="mailto:goudarziha@gmail.com">goudarziha@gmail.com</a></p>
+	<div class="footer-content">
+		<ul>
+			<li>
+				<a href="https://twitter.com/goudarziha">Twitter</a>
+			</li>
+			<li>
+				<a href="https://instagram.com/goudarziha">Instagram</a>
+			</li>
+			<li>
+				<a href="mailto:goudarziha@gmail.com">Email</a>
+			</li>
+		</ul>
+	</div>
 </footer>
 
 <style>
 	footer {
-		width: 100%;
-		justify-content: center;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: auto;
+		width: 100vw;
+		padding-top: 40px;
+		color: #fff;
+	}
+
+	.footer-content {
+		display: flex;
 		align-items: center;
-		padding: 2rem;
+		justify-content: center;
+		flex-direction: column;
+		text-align: center;
 	}
 	ul li {
 		display: inline;
